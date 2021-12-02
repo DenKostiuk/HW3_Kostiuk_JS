3ef7d2b530b694d7cbdbf1eb02375861c9ba769c
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-12-02 15:09:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 let num = Number(prompt('Введите число'));
 let sum = 0;
 
-if (isNaN(num)) {
-    num = Number(prompt('Invalid. You should enter a number, please enter your number again'));
-    for (let i = 1; i <= num; i += 2) {
-        console.log(i);
-        sum = sum + i;
-    };
-} else {
-    for (let i = 1; i <= num; i += 2) {
-        console.log(i);
-        sum = sum + i;
-    };
+while (isNaN(num)) {
+    num = +prompt('Invalid. You should enter a number, please enter your number again');
+}
+for (let i = 1; i <= num; i += 2) {
+    console.log(i);
+    sum = sum + i;
 };
 
 console.log(sum);
